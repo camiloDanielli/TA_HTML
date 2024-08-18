d8a8bb63131a5f82962149e6e1c54286a1e81881
camiloDanielli <daniellicamilo6@gmail.com>
2024-08-18 19:19:08 -0300

    TAs finales CSS

---
+++ JavaScript/TA1/repeatString.js
@@ -0,0 +1,10 @@
+
+
+function repeatString(texto, repeticiones) {
+    for (let i = 0; i < repeticiones; i++) {
+        console.log(texto);
+    }
+}
+
+// Ejemplo de uso
+repeatString("Hola, mundo!", 5);
